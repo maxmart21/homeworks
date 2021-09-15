1883d273599c20e5cf2e73719f385ac459431734
пк <maxmart21@mail.ru>
2021-09-15 21:55:22 +0400

    Changing Number8

--- src/pro/sky/java/course1/lesson6/Number5.java
+++ src/pro/sky/java/course1/lesson6/Number5.java
@@ -2,18 +2,16 @@ package pro.sky.java.course1.lesson6;
 
 public class Number5 {
     public static void main(String[] args) {
-        String fullName = "Ivanov-Ivan-Ivanovich";
-        int lastNameIndex = fullName.indexOf("Ivanov");
-        int lastNameLastIndex = fullName.lastIndexOf("Ivanov");
-        String lastName = fullName.substring(lastNameIndex, lastNameLastIndex - 6);
+        String fullName = "Ivanov Ivan Ivanovich";
+        int spaceIndex1 = fullName.indexOf(' ');
+        int spaceIndex2 = fullName.lastIndexOf(' ');
+        String lastName = fullName.substring(0, spaceIndex1);
         System.out.println("Фамилия сотрудника – " + lastName);
-        int firstNameIndex = fullName.indexOf("Ivan", 6);
-        int firstNameLastIndex = fullName.lastIndexOf("Ivan");
-        String firstName = fullName.substring(firstNameIndex, firstNameLastIndex - 1);
+
+        String firstName = fullName.substring(spaceIndex1 + 1, spaceIndex2);
         System.out.println("Имя сотрудника – " + firstName);
-        int middleNameIndex = fullName.indexOf("Ivanovich", 11);
-        int middleNameLastIndex = fullName.lastIndexOf("");
-        String middleName = fullName.substring(middleNameIndex, middleNameLastIndex);
+
+        String middleName = fullName.substring(spaceIndex2 + 1);
         System.out.println("Отчество сотрудника – " + middleName);
     }
 }
--- src/pro/sky/java/course1/lesson6/Number6.java
+++ src/pro/sky/java/course1/lesson6/Number6.java
@@ -1,13 +1,16 @@
 package pro.sky.java.course1.lesson6;
 
-import java.util.Locale;
-
 public class Number6 {
     public static void main(String[] args) {
         String fullName = "ivanov ivan ivanovich";
-        String result1 = fullName.substring(0, 1).toUpperCase(Locale.ROOT) + fullName.substring(1, 6);
-        String result2 = result1 + fullName.substring(6, 8).toUpperCase(Locale.ROOT) + fullName.substring(8, 11);
-        String result3 = result2 + fullName.substring(11, 13).toUpperCase(Locale.ROOT) + fullName.substring(13, 21);
-        System.out.println("Верное написание ФИО сотрудника с заглавных букв –  " + result3);
+        String[] words = fullName.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder sb = new StringBuilder(words[i]);
+
+            sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
+            words[i] = sb.toString();
+
+        }
+        System.out.println("Верное написание ФИО сотрудника с заглавных букв – " + String.join(" ", words));
     }
 }
