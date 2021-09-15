b25c2a5c9d02a0a3f17742c047263806b43ab7af
пк <maxmart21@mail.ru>
2021-09-15 20:03:38 +0400

    Changing Number8

--- src/pro/sky/java/course1/lesson5/Number8.java
+++ src/pro/sky/java/course1/lesson5/Number8.java
@@ -1,18 +1,16 @@
 package pro.sky.java.course1.lesson5;
 
-import java.util.Arrays;
-
 public class Number8 {
     public static void main(String[] args) {
         int[] arr = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
-        Arrays.sort(arr);
         for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] + arr[i + 1] == -2) {
-                System.out.println(arr[i]);
-                System.out.println(arr[i + 1]);
+            for (int b = i + 1; b < arr.length; b++) {
+                if (arr[i] + arr[b] == -2) {
+                    System.out.println(arr[i] + " " + arr[b]);
+                }
             }
         }
     }
 }
-// Код не работает, но я не вижу проблему. Сдаю это дз таким сырым, потому что к сожалению больше нет времени над ним работать, необходимо приступить к следующему дз.
+
 
