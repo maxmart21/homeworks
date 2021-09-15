0ef3f70231b2332b2ab4b31131a00b506b55358b
пк <maxmart21@mail.ru>
2021-09-15 20:32:28 +0400

    Changing Number8

--- src/pro/sky/java/course1/lesson5/Number6.java
+++ src/pro/sky/java/course1/lesson5/Number6.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class Number6 {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        int[] arr2 = {1, 2, 3, 4, 5};
-        for (int i = 0; i < arr2.length; i++) {
-            arr[i] = arr2[i];
-            System.out.println(Arrays.toString(arr));
+        int[] arr2 = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr2[i] = arr[arr.length - 1 - i];
         }
+        System.out.println(Arrays.toString(arr2));
     }
 }
-// Не придумал другого решения. Это решение неверное скорее всего. Жду подсказки)
+
